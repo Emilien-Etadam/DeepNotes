4d20466ab2279d0faac6bfead719a7f7e7f83d11
Emilien-Etadam <emilien@etadam.com>
2026-02-19 15:33:52 +0100

    fix: patch build for local dev (quasar, sass, ts, prettier, env, browser target)

--- apps/client/quasar.config.js
+++ apps/client/quasar.config.js
@@ -98,7 +98,7 @@ module.exports = configure(function (ctx) {
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#build
     build: {
       target: {
-        browser: ['es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1'],
+        browser: ['es2022', 'edge100', 'firefox100', 'chrome100', 'safari15'],
         node: 'node16',
       },
 
